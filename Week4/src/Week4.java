2cc393b94b5f14ed241aaf6a3300da28ffb22565
SAC-CS112-Christin-Walder <cdubs808@gmail.com>
2015-09-21 22:48:44 -0700

    Initial Check-in

---
+++ Week4/src/Week4.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class Week4 
+{
+	public static void main(String[] args)
+	{
+		Scanner input = new Scanner(System.in);
+		
+		int number1;
+		int number2;
+		int sum;
+		int difference;
+		int product;
+		int quotient;
+		
+		System.out.print("Enter first integer: ");
+		number1=input.nextInt();
+		
+		System.out.print("Enter second integer: ");
+		number2=input.nextInt();
+		
+		sum = number1 + number2;
+		difference = number1 - number2;
+		product = number1 * number2;
+		quotient = number1 / number2;
+		
+		System.out.printf("Sum: %d\n", sum);
+		System.out.printf("Difference: %d\n", difference);
+		System.out.printf("Product: %d\n", product);
+		System.out.printf("Quotient: %d\n", quotient);
+	}
+
+}
